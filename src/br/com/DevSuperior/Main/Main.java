3df3b1bbbc23c2259447d5d0ad4fabcc6a64361e
jijunior <jijunior1026@gmail.com>
2023-10-22 08:06:39 -0300

    Interface para validação de saque incluída

--- src/br/com/DevSuperior/Main/Main.java
+++ src/br/com/DevSuperior/Main/Main.java
@@ -4,15 +4,17 @@ import br.com.DevSuperior.Model.ContaBancaria;
 
 public class Main {
     public static void main(String[] args) {
-        ContaBancaria contaJunior=new ContaBancaria();
+        ContaBancaria contaJunior = new ContaBancaria();
 //        contaJunior.depositar(50);
 //        contaJunior.depositar(10);
 //        contaJunior.depositar(40);
+
         contaJunior.cadastrar();
         contaJunior.depositar();
         contaJunior.sacar();
         contaJunior.sacar();
         contaJunior.sacar();
-        contaJunior.extrato();
+
+
     }
 }
--- src/br/com/DevSuperior/Model/ContaBancaria.java
+++ src/br/com/DevSuperior/Model/ContaBancaria.java
@@ -2,6 +2,7 @@ package br.com.DevSuperior.Model;
 
 import br.com.DevSuperior.Interfaces.AcoesBancarias;
 import br.com.DevSuperior.Interfaces.ExtratoBancario;
+import br.com.DevSuperior.Interfaces.ValidarAcoes;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -9,72 +10,75 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 
-public class ContaBancaria extends Bank implements AcoesBancarias, ExtratoBancario {
+public class ContaBancaria extends Bank implements AcoesBancarias, ExtratoBancario, ValidarAcoes {
     //atributos
 
 
-  //Construtor
+    //Construtor
 
 
     public ContaBancaria() {
 
     }
-    public void cadastrar(){
-        Scanner sc=new Scanner(System.in);
 
-        System.out.println("== DADOS CADASTRAIS ==");
-        System.out.print("Nome do titular: ");
+    public void cadastrar() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\u001B[33m== DADOS CADASTRAIS ==\u001B[m");
+        System.out.print("\u001B[33mNome do titular: \u001B[m");
         setTitular(sc.nextLine());
-        System.out.print("CPF: ");
+        System.out.print("\u001B[33mCPF: \u001B[m");
         setCpf(sc.next());
 
 
-        System.out.println("== DADOS DO CLIENTE ==");
-        System.out.println("Titular: "+getTitular().toUpperCase());
-        System.out.println("Numero: "+getNumero());
-        System.out.println("CPF: "+getCpf());
-        System.out.println("Saldo R$: "+getSaldo());
-        System.out.println("Limite de saque diário R$: "+getLimiteSaque());
-        System.out.println("Conta criada com sucesso!");
-        System.out.println("-".repeat(20));
+        System.out.println("\u001B[33m== DADOS DO CLIENTE ==\u001B[m");
+        System.out.println("\u001B[33mTitular: \u001B[m" + getTitular().toUpperCase());
+        System.out.println("\u001B[33mNumero: \u001B[m" + getNumero());
+        System.out.println("\u001B[33mCPF: \u001B[m" + getCpf());
+        System.out.println("\u001B[33mSaldo R$: \u001B[m" + getSaldo());
+        System.out.println("\u001B[33mLimite de saque diário R$: \u001B[m" + getLimiteSaque());
+        System.out.println("\u001B[34mConta criada com sucesso!\u001B[m");
+        System.out.println("\u001B[33m-\u001B[m".repeat(20));
     }
 
     @Override
     public void depositar() {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Quanto deseja depositar? R$:");
-        double quantia=sc.nextDouble();
-        setSaldo(getSaldo()+quantia);
-        System.out.println("Depósito de R$:"+quantia);
-        System.out.println("Seu saldo atual é R$:"+getSaldo());
-        System.out.println("-".repeat(20));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\u001B[33mQuanto deseja depositar? R$:\u001B[m");
+        double quantia = sc.nextDouble();
+        setSaldo(getSaldo() + quantia);
+        System.out.println("\u001B[33mDepósito de R$:\u001B[m" + quantia);
+        System.out.println("\u001B[33mSeu saldo atual é R$:\u001B[m" + getSaldo());
+        System.out.println("\u001B[33m-\u001B[m".repeat(20));
     }
 
     @Override
     public void sacar() {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Quanto deseja sacar? R$:");
-        double sacarValor=sc.nextDouble();
+        //Tenta executar essa instrução
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("\u001B[33mQuanto deseja sacar? R$:\u001B[m");
+            double sacarValor = sc.nextDouble();
 
+            //metodo da interface,"validar"
+            validarSaque(sacarValor);
 
-        if(sacarValor<=getLimiteSaque()&&sacarValor<=getLimiteSaque()){
-            setSaldo((getSaldo()-sacarValor));
-            System.out.println("Saque de R$: "+sacarValor);
-            System.out.println("Seu saldo atual é R$: "+getSaldo());
-            System.out.println("-".repeat(20));
-        } else if (getSaldo()<sacarValor) {
-            System.out.println("Erro de Saque:Saldo insuficiente!");
-        } else{
-            System.out.println("Erro de Saque:Valor acima do limite permitido!");
-
+            setSaldo((getSaldo() - sacarValor));
+            System.out.println("\u001B[33mSaque de R$: \u001B[m" + sacarValor);
+            System.out.printf("\u001B[36mSeu saldo atual é R$ %.2f%n: \u001B[m" , getSaldo());
+            System.out.println("\u001B[33m-\u001B[m".repeat(20));
+        } catch (RuntimeException e) {
+            //Aqui ele exibe o que foi armazenado na variavel "e",que foi definido no cath
+            System.out.println(e.getMessage());
         }
 
     }
 
+
     @Override
     public void extrato() {
         //Mostrar a data atual
-        LocalDate dataAtual= LocalDate.now();
+        LocalDate dataAtual = LocalDate.now();
         //Exibe a data no formato brasileiro
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String dataFormatada = dataAtual.format(formatter);
@@ -84,17 +88,33 @@ public class ContaBancaria extends Bank implements AcoesBancarias, ExtratoBancar
         DateTimeFormatter horarioOPeracao = DateTimeFormatter.ofPattern("HH:mm:ss");
         String horaFormatada = horaAtual.format(horarioOPeracao);
 
-        System.out.println("== EXTRATO BANCARIO ==");
-        System.out.println("Data da operação: "+dataFormatada);
-        System.out.println("Horário da operação: " + horaFormatada);
+        System.out.println("\u001B[33m== EXTRATO BANCARIO ==\u001B[m");
+        System.out.println("\u001B[33mData da operação: \u001B[m" + dataFormatada);
+        System.out.println("\u001B[33mHorário da operação: \u001B[m" + horaFormatada);
 
-        System.out.println("Cliente: "+getTitular().toUpperCase());
-        System.out.println("Numero: "+getNumero());
-        System.out.println("CPF: "+getCpf());
-        System.out.println("Saldo R$: "+getSaldo());
-        System.out.println("Limite de saque diário R$: "+getLimiteSaque());
+        System.out.println("\u001B[33mCliente: \u001B[m" + getTitular().toUpperCase());
+        System.out.println("\u001B[33mNumero: \u001B[m" + getNumero());
+        System.out.println("\u001B[33mCPF: \u001B[m" + getCpf());
+        System.out.println("\u001B[33mSaldo R$: \u001B[m" + getSaldo());
+        System.out.println("\u001B[33mLimite de saque diário R$: \u001B[m" + getLimiteSaque());
 
         System.out.println("-".repeat(20));
+
+    }
+
+
+    @Override
+    public void validarSaque(double valor) {
+
+        if (valor > getSaldo()) {
+            throw new RuntimeException("\u001B[31mErro de Saque:Saldo insuficiente!\u001B[m");
+
+        }
+        if (valor > getLimiteSaque()) {
+            throw new RuntimeException("\u001B[31mErro de Saque:Valor acima do limite permitido!\u001B[m");
+
+        }
+
 
     }
 }
